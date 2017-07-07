2194be026f70523bb5d4465a512a150e67c97b21
newsjet <newsjet@newsjetdeMac-mini.local>
2017-07-07 18:49:17 +0800

    留言板修改参数

--- videoDetail/src/components/idol/messageBoard.vue
+++ videoDetail/src/components/idol/messageBoard.vue
@@ -121,7 +121,7 @@
                 http.get('/post/list',{
                     params: {
                         targetType: 3,
-                        targetId: self.$route.query.idolId,
+                        targetId: self.$route.query.targetId,
                         from: self.start,
                         rows: self.num
                     }
@@ -168,7 +168,7 @@
                 http.get('/post/list',{
                     params: {
                         targetType: 3,
-                        targetId: self.$route.query.idolId,
+                        targetId: self.$route.query.targetId,
                         from: 0,
                         rows: self.num
                     }
