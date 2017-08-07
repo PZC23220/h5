0fbb747548e1b32d288434f78194a8b5e8114238
Tracy <tracy@groupy.vip>
2017-08-07 12:18:32 +0800

    心理测试app js文件上传

---
+++ groupywebsite/static/index.js
@@ -0,0 +1,5 @@
+var link = document.createElement('link');
+link.type = "text/css"; 
+link.rel = "stylesheet"; 
+link.href = 'http://groupy.vip/static/index.css'; 
+document.getElementsByTagName( "head" )[0].appendChild( link ); 
