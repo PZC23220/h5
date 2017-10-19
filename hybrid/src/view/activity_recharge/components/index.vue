d7e2cdb8874c12c38dca035ad3fefd59990e9760
Tracy <tracy@groupy.vip>
2017-10-19 14:57:17 +0800

    充值活动分享测试

--- hybrid/src/view/activity_recharge/components/index.vue
+++ hybrid/src/view/activity_recharge/components/index.vue
@@ -4,7 +4,7 @@
             <img src="http://photodebug.oss-cn-hongkong.aliyuncs.com/acticity_banner/banner-recharge.jpg" class="banner">
             <!-- <h2>{{activity.theme}}</h2> -->
             <div class="activity_time">
-                <p class="share-sns"><span @click="shareSns('share_facebook')"><img src="http://photodebug.oss-cn-hongkong.aliyuncs.com/sns/icon_facebook_1.png"><i>シェア</i></span><span @click="shareSns('share_twitter')"><img src="http://photodebug.oss-cn-hongkong.aliyuncs.com/sns/icon_twitter_1.png"><i>ツィート</i></span></p>
+                <p class="share-sns" v-if="false"><span @click="shareSns('share_facebook')"><img src="http://photodebug.oss-cn-hongkong.aliyuncs.com/sns/icon_facebook_1.png"><i>シェア</i></span><span @click="shareSns('share_twitter')"><img src="http://photodebug.oss-cn-hongkong.aliyuncs.com/sns/icon_twitter_1.png"><i>ツィート</i></span></p>
                 <h4>{{activity.time}}</h4>
                 <div class="duration_time">
                     <p><span>{{activity.start}}</span><i>2017年10月20日（金）11:00:00</i></p>
@@ -91,7 +91,6 @@
                 var description = `Groupyで購入したコインはもれなく100%増量！チャンスをつかんでコインを沢山手に入れよう...`;
                 var shareImg = `http://photodebug.oss-cn-hongkong.aliyuncs.com/optupload/%E5%85%85%E5%80%BC.jpg`;
                 window.setupWebViewJavascriptBridge(function(bridge) {
-                    alert('shareURL')
                     bridge.callHandler(val, {'title':title,'description':description,'shareImg':shareImg,'shareURL':shareURL})
                 })
           },
