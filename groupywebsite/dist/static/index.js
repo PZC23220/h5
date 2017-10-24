f758d5a1a40e18d3df713734dcd27eae3664c0d3
Tracy <tracy@groupy.vip>
2017-10-24 12:34:32 +0900

    代码迁移

--- groupywebsite/dist/static/index.js
+++
@@ -1,5 +0,0 @@
-var link = document.createElement('link');
-link.type = "text/css"; 
-link.rel = "stylesheet"; 
-link.href = 'http://groupy.vip/static/index.css'; 
-document.getElementsByTagName( "head" )[0].appendChild( link ); 
--- groupywebsite/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
-</template>
-<style type="text/css" lang="scss">
-	@import 'styles/reset.css';
-</style>
